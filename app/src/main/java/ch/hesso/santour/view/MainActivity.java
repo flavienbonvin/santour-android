c1390ac71e4dc72e44234062be35714d4e8e360d
degir <degirolamo.daniel@gmail.com>
2017-11-22 16:53:31 +0100

    Main-Login-AddTrack only design

---
+++ app/src/main/java/ch/hesso/santour/view/MainActivity.java
@@ -0,0 +1,14 @@
+package ch.hesso.santour.view;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import ch.hesso.santour.R;
+
+public class MainActivity extends AppCompatActivity {
+
+}
