c71935b2d5fe620efb6bd44f2e19340eba8d9cf4
peixotte <peixotterafael@gmail.com>
2017-11-26 22:04:52 +0100

    Track Activity Tab Layout

--- app/src/main/java/ch/hesso/santour/view/TrackPODsListFragment.java
+++ app/src/main/java/ch/hesso/santour/view/TrackPODsListFragment.java
@@ -17,6 +17,7 @@ public class TrackPODsListFragment extends Fragment {
         // Required empty public constructor
     }
 
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
