5daa8162bda4d804284f5ccbe807d115ce2f0ef8
degir <degirolamo.daniel@gmail.com>
2017-11-28 16:03:03 +0100

    Fill list of Track with DB Fill list categoryPOD with DB

--- app/src/main/java/ch/hesso/santour/view/TrackPODDetailsFragment.java
+++ app/src/main/java/ch/hesso/santour/view/TrackPODDetailsFragment.java
@@ -7,13 +7,17 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListView;
 
+import java.util.ArrayList;
+
 import ch.hesso.santour.R;
 import ch.hesso.santour.adapter.CategoryListAdapter;
+import ch.hesso.santour.db.CategoryPODDB;
+import ch.hesso.santour.db.DBCallback;
+import ch.hesso.santour.model.CategoryPOD;
 
 public class TrackPODDetailsFragment extends Fragment {
 
-    private ListView listView;
-    private CategoryListAdapter adapter;
+    private View rootView;
 
     public TrackPODDetailsFragment() {
         // Required empty public constructor
@@ -23,6 +27,16 @@ public class TrackPODDetailsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_track_pod_details, container, false);
+       rootView = inflater.inflate(R.layout.fragment_track_pod_details, container, false);
+        CategoryPODDB.getAll(new DBCallback() {
+            @Override
+            public void resolve(Object o) {
+                ArrayList<CategoryPOD> listPodCategory = (ArrayList<CategoryPOD>)o;
+                ListView list = rootView.findViewById(R.id.track_pod_details_categories_list);
+                list.setAdapter(new CategoryListAdapter(TrackPODDetailsFragment.this.getContext(), listPodCategory));
+            }
+        });
+
+        return rootView;
     }
 }
