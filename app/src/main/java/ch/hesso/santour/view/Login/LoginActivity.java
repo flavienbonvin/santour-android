48905e6127a50b66c1ad709580f36c7145638eaf
maxime <maxime.betrisey@gmail.com>
2017-12-12 16:44:12 +0100

    ajout du login

--- app/src/main/java/ch/hesso/santour/view/Login/LoginActivity.java
+++ app/src/main/java/ch/hesso/santour/view/Login/LoginActivity.java
@@ -2,10 +2,24 @@ package ch.hesso.santour.view.Login;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
+import android.widget.AutoCompleteTextView;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import ch.hesso.santour.R;
 import ch.hesso.santour.view.Main.MainActivity;
@@ -15,11 +29,22 @@ import ch.hesso.santour.view.Main.MainActivity;
  */
 public class LoginActivity extends AppCompatActivity {
 
+    private FirebaseAuth auth;
+
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
+        auth = FirebaseAuth.getInstance();
         setContentView(R.layout.login_activity);
         setTitle("Login");
         super.onCreate(savedInstanceState);
+
+        if (auth.getCurrentUser() != null) {
+
+            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+            startActivity(intent);
+            finish();
+        }
 
         /*Test t = new Test();
         t.launch();*/
@@ -29,9 +54,32 @@ public class LoginActivity extends AppCompatActivity {
 
             public void onClick(View v) {
 
-                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-                startActivity(intent);
+                AutoCompleteTextView emailView = findViewById(R.id.login_textView_username);
+                EditText passView = findViewById(R.id.login_textView_password);
+
+                auth.signInWithEmailAndPassword(emailView.getText().toString(), passView.getText().toString())
+                .addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        // If sign in fails, display a message to the user. If sign in succeeds
+                        // the auth state listener will be notified and logic to handle the
+                        // signed in user can be handled in the listener.
+                        //progressBar.setVisibility(View.GONE);
+                        if (!task.isSuccessful()) {
+                            // there was an error
+                            Toast.makeText(LoginActivity.this, "auth failed", Toast.LENGTH_LONG).show();
+
+                        } else {
+                            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                            startActivity(intent);
+                            finish();
+                        }
+                    }
+                });
+
             }
         });
     }
+
+
 }
