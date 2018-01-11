95f2f91ec2ca5bdad32a00960696941f52ed1251
peixotte <peixotterafael@gmail.com>
2018-01-11 16:59:07 +0100

    About fragment

---
+++ app/src/main/java/ch/hesso/santour/view/Main/AboutFragment.java
@@ -0,0 +1,26 @@
+package ch.hesso.santour.view.Main;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import ch.hesso.santour.R;
+
+public class AboutFragment extends Fragment {
+
+    public AboutFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        final View rootView = inflater.inflate(R.layout.fragment_about, container, false);
+        return rootView;
+    }
+}
