33517dfa42806154fbfcf513289a670760e87a18
flavienbonvin <bonvin.flavien@gmail.com>
2018-01-11 17:13:16 +0100

    release 1.1

--- app/src/main/java/ch/hesso/santour/view/Main/SettingsFragment.java
+++ app/src/main/java/ch/hesso/santour/view/Main/SettingsFragment.java
@@ -18,11 +18,16 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.Locale;
 
 import ch.hesso.santour.R;
 import ch.hesso.santour.business.PreferenceDownload;
+import ch.hesso.santour.db.CategoryPODDB;
+import ch.hesso.santour.db.CategoryPOIDB;
+import ch.hesso.santour.db.DBCallback;
+import ch.hesso.santour.db.TrackDB;
 
 public class SettingsFragment extends Fragment {
 
@@ -30,7 +35,7 @@ public class SettingsFragment extends Fragment {
     private FragmentManager fragmentManager;
 
 
-    private final String[] lang = {"fr", "en", "de"};
+    private final String[] lang = {"fr", "en"};
 
     private Spinner spinner;
 
@@ -52,12 +57,10 @@ public class SettingsFragment extends Fragment {
         getActivity().setTitle(R.string.settings);
         setHasOptionsMenu(true);
 
-
-
         final SharedPreferences sharedPref = SettingsFragment.this.getActivity().getPreferences(Context.MODE_PRIVATE);
-
         final TextView minDistance = rootView.findViewById(R.id.settings_tv_minimal_dist_value);
 
+        spinner = rootView.findViewById(R.id.settings_spinner_language);
 
         Button buttonDownload = rootView.findViewById(R.id.bt_settings_server_download_file);
         buttonDownload.setOnClickListener(new View.OnClickListener() {
@@ -70,22 +73,23 @@ public class SettingsFragment extends Fragment {
             }
         });
 
-        minDistance.setText(sharedPref.getString("minimalDistance", "Missing"));
-
-        spinner = rootView.findViewById(R.id.settings_spinner_language);
-
-        String lang = PreferenceManager.getDefaultSharedPreferences(getContext()).getString("lang", "fr");
-        switch (lang){
-            case "fr":
-                spinner.setSelection(0);
-                break;
-            case "en":
-                spinner.setSelection(1);
-                break;
-            case "de":
-                spinner.setSelection(2);
-                break;
-        }
+        Button buttonSync = rootView.findViewById(R.id.settings_button_synchro);
+        buttonSync.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                CategoryPODDB.getAll(new DBCallback() {
+                    @Override
+                    public void resolve(Object o) {
+                        CategoryPOIDB.getAll(new DBCallback() {
+                            @Override
+                            public void resolve(Object o) {
+                                Toast.makeText(MainActivity.mainActivity.getBaseContext(), R.string.firebase_download, Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                    }
+                });
+            }
+        });
 
         return rootView;
     }
